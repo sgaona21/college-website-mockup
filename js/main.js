40fcb4d61bdd4cfd1c57f79c6b23addb30bd4168
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-04-12 15:57:31 -0700

    lunch break

--- js/main.js
+++ js/main.js
@@ -7,5 +7,34 @@ function heyLOL() {
     console.log("hey LOL")
 }
 
-heyLOL();
+
+// ********** FORM ********** //
+const firstNameInput = document.querySelector('.first-name input[type="text"]');
+
+const nameRegEx = /^[A-Za-z]{2,}$/;
+
+function inputValidation(tag, regex, validString, invalidString) {
+    let regexValid = regex.test(tag.value);
+
+    if (tag.value === "") {
+        document.querySelector(validString).style.display = "none"
+        document.querySelector(invalidString).style.display = "none"
+    }
+    else if (regexValid) {
+        document.querySelector(validString).style.display = "inline"
+        document.querySelector(invalidString).style.display = "none"
+    } else {
+        document.querySelector(invalidString).style.display = "inline"
+        document.querySelector(validString).style.display = "none"
+    }
+}
+
+firstNameInput.addEventListener('blur', () => {
+    inputValidation(firstNameInput, nameRegEx, ".valid", ".invalid");
+})
+
+
+
+
+
 
