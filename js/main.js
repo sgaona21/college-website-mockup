1710b0084eed1bdd24297da6ab347aa7decdfffc
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-04-14 16:35:55 -0700

    comments for tuition js complete

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ inStateSelection = document.getElementById('in-state');
 outStateSelection = document.getElementById('out-state');
 
 function calculateTuition(tuition) {
+    // this function takes the user selection and multiplies it by in state or out of state tuition cost. it stores the selection as a variable and converts it to an integer. it then calculates the total. it then targets the span element and updates the text with the total amount that is convereted to a formatted string that includes commas for thousands place using toLocaleString()
     if (tuition == 5000) {
         let semesters = parseInt(inStateSelection.value);
         semesters = semesters * tuition;
@@ -19,6 +20,7 @@ function calculateTuition(tuition) {
     }
 }
 
+// this code attaches the event listeners to the semester selections and invokes the in state or out of state tuition cost and updates the total to the user on the screen. 
 inStateSelection.addEventListener('change', () => {
     calculateTuition(5000);
 });
@@ -32,7 +34,6 @@ outStateSelection.addEventListener('change', () => {
 
 
 // ********** APPLICATION PAGE ********** //
-// here all of my dom elements assigned to variables: 
 const form = document.getElementById("form");
 const firstNameInput = document.querySelector('.first-name input[type="text"]');
 const lastNameInput = document.querySelector('.last-name input[type="text"]');
